2ed406bef1b9436399afd00d34699391b787d162
Golam Rabbi <spirabbi@gmail.com>
2021-07-23 11:12:03 +0600

    Dynamic link setup

--- resources/views/frontend/partials/link.blade.php
+++ resources/views/frontend/partials/link.blade.php
@@ -1,4 +1,4 @@
 <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-<link rel="stylesheet" href="asset/css/swiper-bundle.min.css">
-<link rel="stylesheet" href="asset/css/style.css">
+<link rel="stylesheet" href="{{ asset('asset/css/swiper-bundle.min.css') }}">
+<link rel="stylesheet" href="{{ asset('asset/css/style.css') }}">
--- resources/views/frontend/partials/script.blade.php
+++ resources/views/frontend/partials/script.blade.php
@@ -1,4 +1,4 @@
     <!-- Option 1: Bootstrap Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-    <script src="asset/js/swiper-bundle.min.js"></script>
-    <script src="asset/js/main.js"></script>
+    <script src="{{ asset('asset/js/swiper-bundle.min.js') }}"></script>
+    <script src="{{ asset('asset/js/main.js') }}"></script>
